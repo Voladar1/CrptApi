8cf500b448a8c82ef2558d74b6b4dddac7bef714
Voladar1 <elrond169@gmail.com>
2024-08-21 00:16:28 +0300

    Added document classes

--- src/main/java/org/example/CrptApi.java
+++ src/main/java/org/example/CrptApi.java
@@ -24,8 +24,26 @@ public class CrptApi {
     public static void main(String[] args) {
         var crptApi = new CrptApi(TimeUnit.MINUTES,4);
         var objectTest = "{\"description\":{\"participantInn\":\"string\"},\"doc_id\":\"string\",\"doc_status\":\"string\",\"doc_type\":\"LP_INTRODUCE_GOODS\",\"importRequest\":true,\"owner_inn\":\"string\",\"participant_inn\":\"string\",\"producer_inn\":\"string\",\"production_date\":\"2020-01-23\",\"production_type\":\"string\",\"products\":[{\"certificate_document\":\"string\",\"certificate_document_date\":\"2020-01-23\",\"certificate_document_number\":\"string\",\"owner_inn\":\"string\",\"producer_inn\":\"string\",\"production_date\":\"2020-01-23\",\"tnved_code\":\"string\",\"uit_code\":\"string\",\"uitu_code\":\"string\"}],\"reg_date\":\"2020-01-23\",\"reg_number\":\"string\"}";
+        var document = new Document();
+        document.doc_id = "133";
+        document.doc_status = "125";
+        document.doc_type = "123";
+        document.importRequest = true;
+        document.owner_inn = "123";
 
-        Runnable task = () -> crptApi.sendObject(objectTest, "Signature");
+        var product = new Product();
+        product.production_date = "2024-04-31";
+        product.certificate_document = "license";
+        product.owner_inn = "Boss";
+        product.producer_inn = "Valera";
+        document.products = new ArrayList<Product>();
+        document.products.add(product);
+
+        var description = new Description();
+        description.participantInn = "ring";
+        document.description = description;
+
+        Runnable task = () -> crptApi.sendObject(document, "Signature");
         Runnable task2 = () -> {
             try {
                 Thread.sleep(61000);
@@ -64,16 +82,13 @@ public class CrptApi {
 
             requestCount.incrementAndGet();
 
-            // Populate your Document object here
-            //Document document = new Document();
-
             var body = createRequestBody(document);
 
             if (body == null) {
                 return;
             }
 
-            System.out.println("Body created");
+            System.out.println("Body created: " + body);
             postDocument(body);
 
             System.out.println("Document created with signature: " + signature);
@@ -84,8 +99,6 @@ public class CrptApi {
 
     private String createRequestBody(Object document) {
         ObjectMapper objectMapper = new ObjectMapper();
-        // to serialize object
-        //String json = objectMapper.writeValueAsString(document);
         try {
             return objectMapper.writeValueAsString(document);
         } catch (JsonProcessingException e) {
@@ -105,5 +118,37 @@ public class CrptApi {
         } catch (IOException e) {
             System.out.println("Unexpected error on httpClient: " + e.getMessage());
         }
+    }
+
+    public static class Description{
+        public String participantInn;
+    }
+
+    public static class Product{
+        public String certificate_document;
+        public String certificate_document_date;
+        public String certificate_document_number;
+        public String owner_inn;
+        public String producer_inn;
+        public String production_date;
+        public String tnved_code;
+        public String uit_code;
+        public String uitu_code;
+    }
+
+    public static class Document{
+        public Description description;
+        public String doc_id;
+        public String doc_status;
+        public String doc_type;
+        public boolean importRequest;
+        public String owner_inn;
+        public String participant_inn;
+        public String producer_inn;
+        public String production_date;
+        public String production_type;
+        public ArrayList<Product> products;
+        public String reg_date;
+        public String reg_number;
     }
 }
